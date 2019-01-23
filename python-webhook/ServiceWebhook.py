3e5e0da34be4a8a5671ca99b10219fd4ece9c5e6
satomic <yxfdental@126.com>
2019-01-23 19:17:15 +0800

    add python-webhook

---
+++ python-webhook/ServiceWebhook.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+from MicroService.Service import BaseService
+import os
+import datetime
+
+class ServiceFace(BaseService):
+
+    TIMEOUT = 100
+
+    def __init__(self, uri="/webhook", service_name="webhook"):
+        BaseService.__init__(self, uri=uri, service_name=service_name, ver = "v0.0.1", port = 80)
+
+    def process(self, dictReq):
+        # ret = "hello!", self.serviceCall("http://127.0.0.1:5001/test",dictReq) # call other microservice
+        # dictReq = {
+        #  "type": "local/url",
+        #  "path": "path"
+        # }
+        log = "%s %s" % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],dictReq)
+        print(log.replace("'",'"').replace('u"','"'))
+
+if __name__ == "__main__":
+    print("webhook service started, please post info to http://<SERVER_IP>/webhook")
+    ms = ServiceFace()
+    ms.start()
