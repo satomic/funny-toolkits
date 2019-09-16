b5c78ded87c585870dfc192a02f3a82a8c21f2d8
Xuefeng <yxfdental@126.com>
2019-09-16 12:06:56 +0800

    Update kafka-client.py

--- python-kafka/kafka-client.py
+++ python-kafka/kafka-client.py
@@ -51,4 +51,4 @@ if ROLE == "c":
     for message in consumer:
         print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                               message.offset, message.key,
-                                              message.value))
+                                              message.value.decode('utf8')))
